5d2773bc3e52f69b19e9969fa7283dbdc413f5e8
Marija Kalebota Kodzoman <marija.kalebota@gmail.com>
2020-08-06 03:48:40 -0700

    Create tree node model

--- company_model_site/company_model_manager/models.py
+++ company_model_site/company_model_manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Node(models.Model):
+    root = models.ForeignKey('Node', on_delete=models.DO_NOTHING, related_name='+')
+    parent = models.ForeignKey('Node', on_delete=models.DO_NOTHING, related_name='+')
+    height = models.PositiveIntegerField()
--- company_model_site/company_model_site/settings.py
+++ company_model_site/company_model_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'company_model_manager.apps.CompanyModelManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
