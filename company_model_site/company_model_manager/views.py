581543b6f363bfeb563db5aaef10688e84e730ff
Marija Kalebota Kodzoman <marija.kalebota@gmail.com>
2020-08-09 08:37:50 -0700

    Refactor duplicate code

--- company_model_site/company_model_manager/views.py
+++ company_model_site/company_model_manager/views.py
@@ -13,66 +13,66 @@ def index(request):
     return HttpResponse("Hello, world. You're at the index.")
 
 def node_api(request, node_id):
-    try:
-        node = models.Node.objects.get(id=node_id)
-    except models.Node.DoesNotExist as e:
-        return HttpResponse("Object does not exist", status=404)
-    if request.method == "GET":
-        return JsonResponse(node.to_dict())
-    elif request.method == "POST":
-        new_parent_id = request.POST.get("new_parent_id")
-        try:
-            node.set_parent(new_parent_id)
-        except (models.Node.DoesNotExist, ValueError) as e:
-            return HttpResponse(f"Updating parent was unsucessful. Error: {str(e)}", status=400)
-        return JsonResponse(node.to_dict())
+    response = _node(request, node_id, False)
+    if response.status_code >= 400:
+        return HttpResponse(response)
     else:
-        return HttpResponse("Unsupported operation", status=400)
-
+        return HttpResponse(response, content_type="application/json")
 
 def node_gui(request, node_id):
+    response = _node(request, node_id, True)
+    return HttpResponse(response)
+
+def _node(request, node_id, gui):
     try:
         node = models.Node.objects.get(id=node_id)
     except models.Node.DoesNotExist as e:
         return HttpResponse("Object does not exist", status=404)
 
     if request.method == "GET":
-        template = loader.get_template("company_model_manager/nodes.html")
-        context = {"node": node}
-        return HttpResponse(template.render(context, request))
+        if gui:
+            template = loader.get_template("company_model_manager/nodes.html")
+            context = {"node": node}
+            return HttpResponse(template.render(context, request))
+        else:
+            return JsonResponse(node.to_dict())
     elif request.method == "POST":
         new_parent_id = request.POST.get("new_parent_id")
         try:
             node.set_parent(new_parent_id)
         except (models.Node.DoesNotExist, ValueError) as e:
             return HttpResponse(f"Updating parent was unsucessful. Error: {str(e)}", status=400)
-        context = {"node": node}
-        return HttpResponse(template.render(context, request))
+        if gui:
+            context = {"node": node}
+            return HttpResponse(template.render(context, request))
+        else:
+            return JsonResponse(node.to_dict())
     else:
         return HttpResponse("Unsupported operation", status=400)
 
-
 def get_descendants_api(request, node_id):
-    try:
-        node = models.Node.objects.get(id=node_id)
-    except models.Node.DoesNotExist as e:
-        return HttpResponse("Object does not exist", status=404)
-    descendants = node.get_descendants()
-    return JsonResponse(
-        {"descendants": [descendant.to_dict() for descendant in descendants]},
-    )
-
+    response = _get_descendants(request, node_id, False)
+    return HttpResponse(response)
 
 def get_descendants_gui(request, node_id):
+    response = _get_descendants(request, node_id, True)
+    return HttpResponse(response)
+
+def _get_descendants(request, node_id, gui):
     try:
         node = models.Node.objects.get(id=node_id)
     except models.Node.DoesNotExist as e:
         return HttpResponse("Object does not exist", status=404)
     descendants = node.get_descendants()
 
-    template = loader.get_template("company_model_manager/descendants.html")
-    context = {
-        "descendants": descendants,
-        "node_id": node_id,
-    }
-    return HttpResponse(template.render(context, request))
+    if gui:
+        template = loader.get_template("company_model_manager/descendants.html")
+        context = {
+            "descendants": descendants,
+            "node_id": node_id,
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        return JsonResponse(
+        {"descendants": [descendant.to_dict() for descendant in descendants]},
+        )
