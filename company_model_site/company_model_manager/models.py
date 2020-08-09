c1055f5f829311464115850788dc09b5e4c121a0
Marija Kalebota Kodzoman <marija.kalebota@gmail.com>
2020-08-09 05:30:52 -0700

    Move Node logic to model

--- company_model_site/company_model_manager/models.py
+++ company_model_site/company_model_manager/models.py
@@ -1,8 +1,143 @@
 from django.db import models
+from django.db import IntegrityError, transaction
 
+import functools
+import json
 # Create your models here.
 
 class Node(models.Model):
     root = models.ForeignKey('Node', on_delete=models.DO_NOTHING, blank=True, null=True, related_name='+')
     parent = models.ForeignKey('Node', on_delete=models.DO_NOTHING, blank=True, null=True, related_name='+')
     height = models.BigIntegerField()
+
+    @classmethod
+    def insert(cls, parent_id=None):
+        """
+        Create a node in the tree. If the parent is not passed (None), then
+        the nodei s considered the root node. There can only be one root node,
+        and an exception will be raised (ValuError) if this is not true.
+        """
+        if parent_id == None:
+            created, node = cls.insert_root()
+        else:
+            created, node = cls.insert_node(parent_id)
+
+        return created, node
+
+    @classmethod
+    def insert_root(cls):
+        """
+        Inserts the root node if possible. Returns a tuple (created, root),
+        where created is a bool flag indiciated has the root node been created.
+        """
+        if cls.objects.exists():
+            return False, None
+
+        root = cls(
+            root = None,
+            parent = None,
+            height = 0,
+        )
+        try:
+            with transaction.atomic():
+                root.save()
+                root.root = root
+                root.save()
+                return True, root
+        except IntegrityError:
+            return False, None
+
+    @classmethod
+    def insert_node(cls, parent_id):
+        """
+        Insert a non root node and attach it to the parent node. If this 
+        method is called while there is no root node or the parent node id
+        does not exist, it will not insert the node.
+
+        The method returns a tuple (created, node) - where created is a bool
+        flag indicated has the node been created, and the node is a refernece
+        to the node if it has been created, otherwise it is None.
+        """
+        parent = cls.objects.get(id=parent_id)
+        if parent is None:
+            return False, None
+
+        node = cls(
+            root = cls.get_root(),
+            parent = parent,
+            height = parent.height + 1,
+        )
+        node.save()
+        return True, node
+
+    @classmethod
+    @functools.lru_cache()
+    def get_root(cls):
+        """
+        Returns the root node if it exists, otherwise returns None.
+        """
+        if not cls.objects.exists():
+            return None
+
+        # TODO Check if the root actually exists
+        return cls.objects.get(parent=None)
+
+    @classmethod
+    def is_node_among_descendants(cls, top_node, node_to_find):
+        """
+        Checks whether a node is among descendants of a given top node.
+        """
+        if not cls.objects.filter(parent=top_node):
+            return False
+        else:
+            nodes_to_check = [top_node]
+
+            while len(nodes_to_check) != 0:
+                node = nodes_to_check.pop(0)
+                children = cls.objects.filter(parent=node)
+                for child in children:
+                    if child.id == node_to_find.id:
+                        return True
+                    else:
+                        nodes_to_check.append(child)
+            return False
+
+    def set_parent(self, parent_id):
+        """
+        Set a new parent if possible and return flag indicating
+        if the new parent has been set.
+        """
+        new_parent = Node.objects.get(id=parent_id)
+        if self.is_node_among_descendants(self, new_parent):
+            return False
+
+        self.parent = new_parent
+        self.save()
+        return True
+        
+    def get_descendants(self):
+        '''
+        Run BFS to get all descendant nodes.
+        '''
+        # TODO request?
+        # TODO arguments into docstring
+        # TODO invalid node id
+        descendants = []
+        nodes_to_check = [self]
+
+        while len(nodes_to_check) != 0:
+            node = nodes_to_check.pop(0)
+            children = Node.objects.filter(parent=node)
+            for child in children:
+                nodes_to_check.append(child)
+                descendants.append(child)
+        
+        return descendants
+
+    def to_dict(self):
+        return {
+            'id': str(self.id),
+            'parent': str(self.parent.id) if self.parent else str(None),
+            'root': str(self.root.id),
+            'height': str(self.height),
+        }
--- company_model_site/company_model_manager/views.py
+++ company_model_site/company_model_manager/views.py
@@ -6,114 +6,48 @@ from django.db import IntegrityError, transaction
 import functools
 
 from . import models
+from django.core import serializers
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
 
-def insert_root(request):
-    # query DB. If result set is empty, create root
-    #if len(models.Node.objects.all()) == 0:
-    if not models.Node.objects.exists():
-        root = models.Node(
-            root = None,
-            parent = None,
-            height = 0,
-        )
-        try:
-            with transaction.atomic():
-                root.save()
-                root.root = root
-                root.save()
-                return HttpResponse("Root created.", status=201)
-        except IntegrityError:
-            return HttpResponse("Transaction unsuccessful, please try again.", status=400)
-    else:
-        return HttpResponse("Root already exists.", status=400)
-
-@functools.lru_cache()
-def get_root():
-    if models.Node.objects.exists():
-        try:
-            return models.Node.objects.get(parent=None)
-        #except Entry.DoesNotExist:
-        except:
-            # TODO 
-            return None
-    else:
-        # TODO
-        return None
-    
-def insert_node(request, parent_id):
-    # query DB to check if given parent exists
-    parent = models.Node.objects.get(id=parent_id)
-    if parent is not None:
-        parent_height = parent.height
-        new_node = models.Node(
-            root = get_root(),
-            parent = parent,
-            height = parent_height + 1,
-        )
-        new_node.save()
-        return HttpResponse("Node inserted.")
-    else:
-        return HttpResponse("Node with provided ID does not exist.")
-
-def get_node(request, node_id):
+def node_api(request, node_id):
     node = models.Node.objects.get(id=node_id)
-    template = loader.get_template('company_model_manager/nodes.html')
-    context = {
-        'node': node,
-    }
-    return HttpResponse(template.render(context, request))
-
-def node(request, node_id):
     if request.method == "GET":
-        get_node(request, node_id)
+        #return HttpResponse(serializers.serialize('json', [node]))
+        return JsonResponse(node.to_dict())
     elif request.method == "POST":
         new_parent_id = request.POST.get("new_parent_id")
-        set_parent(request, node_id, new_parent_id)
+        node.set_parent(new_parent_id)
+        #return HttpResponse(serializers.serialize('json', [node]))
+        return JsonResponse(node.to_dict())
+    else:
+        return HttpResponse("Unsupported operation", status=400)
+
+def node_gui(request, node_id):
+    if request.method == "GET":
+        node = models.Node.objects.get(id=node_id)
+        template = loader.get_template('company_model_manager/nodes.html')
+        context = {'node': node}
+        return HttpResponse(template.render(context, request))
+    elif request.method == "POST":
+        new_parent_id = request.POST.get("new_parent_id")
+        node.set_parent(new_parent_id)
+        context = {'node': node}
+        return HttpResponse(template.render(context, request))
     # TODO implement DELETE
     else:
         return HttpResponse("Unsupported operation", status=400)
 
-def get_descendant_nodes(node_id):
-    '''
-    Run BFS to get all descendants.
-    '''
-    # TODO request?
-    # TODO arguments into docstring
-    # TODO invalid node id
-    descendants = []
+def get_descendants_api(request, node_id):
     node = models.Node.objects.get(id=node_id)
-    nodes_to_check = [node]
+    descendants = node.get_descendants()
+    #return HttpResponse(serializers.serialize('json', descendants))
+    return JsonResponse({"descendants": [descendant.to_dict() for descendant in descendants]},)
 
-    while len(nodes_to_check) != 0:
-        node = nodes_to_check.pop(0)
-        children = models.Node.objects.filter(parent=node)
-        for child in children:
-            nodes_to_check.append(child)
-            descendants.append(child)
-    
-    return descendants
-
-def get_descendants(request, node_id):
-    descendant_nodes = get_descendant_nodes(node_id)
-    descendants = []
-
-    for descendant_node in descendant_nodes:
-        descendant = {
-            'id': str(descendant_node.id),
-            'parent': str(descendant_node.parent.id),
-            'root': str(descendant_node.root.id),
-            'height': str(descendant_node.height),
-        }
-        descendants.append(descendant)
-    
-    #response_data = {
-    #    'descendants': descendants,
-    #}
-
-    #return JsonResponse(response_data)
+def get_descendants_gui(request, node_id):
+    node = models.Node.objects.get(id=node_id)
+    descendants = node.get_descendants()
 
     template = loader.get_template('company_model_manager/descendants.html')
     context = {
@@ -122,29 +56,13 @@ def get_descendants(request, node_id):
     }
     return HttpResponse(template.render(context, request))
 
-def is_node_among_descendants(top_node, node_to_find):
-    if not models.Node.objects.filter(parent=top_node):
-        return False
-    else:
-        nodes_to_check = [top_node]
-
-        while len(nodes_to_check) != 0:
-            node = nodes_to_check.pop(0)
-            children = models.Node.objects.filter(parent=node)
-            for child in children:
-                if child.id == node_to_find.id:
-                    return True
-                else:
-                    nodes_to_check.append(child)
-        return False
-
 def set_parent(request, node_id, new_parent_id):
     # TODO cover edge cases - when trying to set parent of root, etc.
     # TODO requests
     node = models.Node.objects.get(id=node_id)
     old_parent_id = node.parent.id
     new_parent = models.Node.objects.get(id=new_parent_id)
-    if is_node_among_descendants(node, new_parent):
+    if models.Node.is_node_among_descendants(node, new_parent):
         return HttpResponse("Illegal operation")
     else:
         node.parent = new_parent
